8638f49739522c3df0baf4f6b1af738dee1d3d62
Pavel Šturc <psturc@redhat.com>
2016-07-08 11:00:43 +0200

    Update run.js

--- run.js
+++ run.js
@@ -2,7 +2,7 @@ var exec = require('child_process').exec;
 
 var runningScript = exec('test.sh', function(err) {
   if (err) {
-    console.log("Error when running a script: ", err);
+    console.error("Error when running a script: ", err);
     return false;
   }
 });
